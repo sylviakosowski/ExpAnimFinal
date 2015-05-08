9a8c295b171a8c440e1bf7d28f0dd4554f9f75b8
Cyphrus <wmaynes@William-Mayness-MacBook-Pro.local>
2015-05-08 15:28:23 -0400

    OpenAnimation reconstructed

---
+++ ExpAnimFinal/Assets/Animations/VideoController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class VideoController : MonoBehaviour {
+	
+	// Use this for initialization
+	void Start () {
+		Renderer rend = GetComponent<Renderer> ();
+		MovieTexture movie = (MovieTexture)rend.material.mainTexture;
+		movie.Play ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Renderer rend = GetComponent<Renderer> ();
+		MovieTexture movie = (MovieTexture)rend.material.mainTexture;
+		if (!movie.isPlaying) {
+			movie.Stop();
+			Debug.Log("Video done. Change Scene.");
+			//Application.LoadLevel("DemoScene");
+		}
+	}
+}
